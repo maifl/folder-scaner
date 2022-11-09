1fa3454b314e7b7941bc8353c014953d8280e760
maifl <59164785+maifl@users.noreply.github.com>
2022-11-09 15:26:55 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+OBSERVATION_FOLDER = r'\\TV-MEDIA1\vid_transfer\APcontent'  # какую папку сканировать на новые файлы
+MOVE_FOLDER = r'\\TV-MEDIA1\!agencies\aptn\temp'  # в какую папку перемещать
+SCAN_SUBFOLDERS = False  # сканировать или нет подпапки (True/False)
+PAUSE_REFRESH_FOLDER = 10  # пауза между просмотром папки на новые файлы. в секундах
+FROM_EMAIL = 'report.ap@mail.ru'  # с какой почты отправлять уведомление
+FROM_PASSWORD = '1488'  # пароль от этой почты
+TO_EMAIL = 'penetrator@mail.ru'  # на какую почту отправлять уведомление
